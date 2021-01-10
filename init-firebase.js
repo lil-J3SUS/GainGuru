89e9b30e0a60cb4a5d2beef424197f27362770b7
Lokus <lukas.tarantl@gmail.com>
2021-01-10 22:36:21 +0100

    -Added script firebase-analytics.js                see index.html
    -changed firebaseConfig to actual config
                   see init-firebase.js
                  -> for questions contact me

--- init-firebase.js
+++ init-firebase.js
@@ -1,6 +1,13 @@
 // TODO: Replace the following with your app's Firebase project configuration
-var firebaseConfig = {
-    // ...
+const firebaseConfig = {
+    apiKey: "AIzaSyDZa7kRYMmLaw9lmWkCcOTAiAiyhpIN5bc",
+    authDomain: "gain-guru.firebaseapp.com",
+    databaseURL: "https://gain-guru-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "gain-guru",
+    storageBucket: "gain-guru.appspot.com",
+    messagingSenderId: "927825000882",
+    appId: "1:927825000882:web:9382883061f77d0c57ccab",
+    measurementId: "G-PLCYRQVZGQ"
 };
 
 // Initialize Firebase
